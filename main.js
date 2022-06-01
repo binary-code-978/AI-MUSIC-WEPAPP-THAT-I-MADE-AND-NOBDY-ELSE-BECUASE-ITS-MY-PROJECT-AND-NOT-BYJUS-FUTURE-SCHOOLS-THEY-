5f1b3ddaf7499acd1826fef293da9fc85784d2c4
binary-code-978 <93479009+binary-code-978@users.noreply.github.com>
2022-06-01 15:26:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -36,8 +36,8 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results + "I hope they work");
-        ScoreLeftWrist = results[0].pose.keypoints[10].score;
-        ScoreRightWrist = results[0].pose.keypoints[9].score;
+        ScoreLeftWrist = results[0].pose.keypoints[9].score;
+        ScoreRightWrist = results[0].pose.keypoints[10].score;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
 
         rightWristX = results[0].pose.rightWrist.x;
@@ -46,5 +46,26 @@ function gotPoses(results) {
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
 
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    fill('#FF0000');
+    stroke('#FF0000');
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWrsitY, 20);
+
+        song2.stop();
+
+        if (song1_status == false) {
+            song1.play();
+            document.getElementById("song").innerHTML = "Playing - Someone I used to know";
+        }
     }
 }
